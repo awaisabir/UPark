8f8cac993ddcfbbe2107a2883bc1b76baafbff99
awaisabir <awaisabirqureshi@gmail.com>
2018-04-07 11:03:33 -0400

    Added Mongo Singleton

---
+++ server/db/Dbi.js
@@ -0,0 +1,45 @@
+const DB_CONFIG = require('../config/db');
+const MongoClient = require('mongodb').MongoClient;
+
+// MongoDB Singleton
+class MongoInterface {
+  constructor() {
+    if(!MongoInterface.instance){
+      MongoClient.connect(DB_CONFIG.db, (err, client) => {
+        this._db = client.db('COMP4601');
+        this._collection = this._db.collection('files');
+      });
+
+      MongoInterface.instance = this;
+    }
+  }
+
+  _doesFileExist(url) {
+    const query = {url};
+
+    return new Promise((resolve, reject) => {
+      this._collection.findOne(query).then(file => {
+        if (file) resolve(true);
+        
+        return resolve(false);
+      }).catch(err => reject(new Error('Something went wrong with the call')));
+    });
+  }
+
+  insertFile(url, callback) {
+    this._doesFileExist(url).then(result => {
+      if (result) {
+        callback('File exists', null);
+      } else {
+        this._collection.insert({url}).then(file => {
+          callback(null, true);
+        }).catch(err => callback(err, null));
+      }
+    });
+  }
+}
+
+const instance = new MongoInterface();
+// Object.freeze(instance);
+
+module.exports = instance;
