c910e32515dbc974a1c760b8fcf6c0e338f02d66
awaisabir <awaisabirqureshi@gmail.com>
2018-04-10 03:00:44 -0400

    added PORT

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const Crawler = require('./models/Crawler');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(err => {
+app.listen(PORT, err => {
   if (err) console.error(err);
 
   console.log(`Server started on port ${PORT}`);
