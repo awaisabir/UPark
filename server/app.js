9fa1e7e7a78ee4c40507463e173d426b9d3ce4be
Pierre <pseguin2011@hotmail.com>
2018-04-11 17:54:17 -0400

    Fixed minor bugs and path issues, added routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express  = require('express');
 
 const Crawler = require('./models/Crawler');
-
+const mainRoute = require('./routes/index.js');
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+app.use('/',mainRoute)
 app.listen(PORT, err => {
   if (err) console.error(err);
 
