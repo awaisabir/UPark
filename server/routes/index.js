05f2140caa392f44749179bc17ce4afa811314e5
Pierre <pseguin2011@hotmail.com>
2018-04-13 22:06:31 -0400

    added the database requests to the CF instead of test data

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const router = express.Router();
-
 const Crawler = require('../models/Crawler');
 const CSVParser = require('../models/CSVParser');
 const UserBasedCF = require('../algo/UserBasedCF');
@@ -36,45 +34,52 @@ router.get('/update', (req, res) => {
   });
 });
 
-router.get('/price/*/*', (req, res) => {
+router.get('/price/*/*', async (req, res) => {
     //determine which lat and long the request belongs to
     let numberOfQuadrants = 3;
-    let testCoords  = [ {lat:-45.03879723, long:75.032837497}, {lat:-45.07228457,long:75.023432657}, {lat:-45.0932857,long:75.144156753}, {lat:-45.1433982,long:75.524823729}, {lat:-45.243987,long:75.824823729}];
-
-    let coordMan = new CoordinateManager();
-    coordMan.setQuadrantsCoordinates(testCoords, numberOfQuadrants);
-    let userLat = coordMan.getLatIndex(req.params[0]);
-    let userLong = coordMan.getLongIndex(req.params[1]);
-    
-    if (userLat === -1 || userLong === -1){
-        res.json({status:false, message:'coordinate was out of range'});
-        return;
-    }
-
-    let matrix = [];
-
-    while(! optimalMatrix) {
-        matrix = [];
-        for (let i = 0; i < coordMan.lats.length-1; i++) {
-            matrix[i] = [];
-            for (let j = 0; j < coordMan.longs.length-1; j++)
-                matrix[i][j] = -1;
+    try{
+      let testCoords  = await Dbi.getCoordinates();//[ {lat:-45.03879723, long:75.032837497}, {lat:-45.07228457,long:75.023432657}, {lat:-45.0932857,long:75.144156753}, {lat:-45.1433982,long:75.524823729}, {lat:-45.243987,long:75.824823729}];
+      console.log(testCoords);
+      let coordMan = new CoordinateManager();
+      coordMan.setQuadrantsCoordinates(testCoords, numberOfQuadrants);
+      let userLat = coordMan.getLatIndex(req.params[0]);
+      let userLong = coordMan.getLongIndex(req.params[1]);
+      
+      if (userLat === -1 || userLong === -1){
+        return res.json({success:false, message:'coordinate was out of range'});
+      }
+  
+      let matrix = [];
+      for (let i = 0; i < coordMan.lats.length-1; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < coordMan.longs.length-1; j++)
+          matrix[i][j] = -1;
+      }
+      for (let i = 0; i < coordMan.lats.length-1; i++)
+        for (let j = 0; j < coordMan.longs.length-1; j++){
+          let tickets = await Dbi.getTicketNumberForQuadrant(coordMan.lats[i],coordMan.lats[i+1], coordMan.longs[j], coordMan.longs[j+1]);
+          if(tickets != 0)
+            matrix[i][j] += tickets;
         }
+    
+  
+  
+  
+      // for(let coordinate of testCoords) {
+      //     let weight = 1;//request the database for the number of tickets issued at the location / the cost of tickets at the location
+      //     console.log(coordinate + ", " + coordMan.getLatIndex(coordinate.Lat) + " : " + coordMan.getLongIndex(coordinate.Long));
+      //     matrix[coordMan.getLatIndex(coordinate.Lat)][coordMan.getLongIndex(coordinate.Long)] += await Dbi.getTicketNumberForQuadrant();
+      // }
+      // console.log(matrix);
+  
+      let uCF = new UserBasedCF(matrix); 
+      let weight = uCF._computeUserBasedPrediction(userLat,userLong);
+      return res.json({success:true, value:weight});
+    }
+    catch(err){
+      console.log(err);
     }
 
-
-
-
-    for(let coordinate of testCoords) {
-        let weight = 1;//request the database for the number of tickets issued at the location / the cost of tickets at the location
-        console.log(coordinate + ", " + coordMan.getLatIndex(coordinate.lat) + " : " + coordMan.getLongIndex(coordinate.long));
-        matrix[coordMan.getLatIndex(coordinate.lat)][coordMan.getLongIndex(coordinate.long)] += weight;
-    }
-    console.log(matrix);
-
-    let uCF = new UserBasedCF(matrix); 
-    let weight = uCF._computeUserBasedPrediction(userLat,userLong);
-    res.json({status:true, value:weight});
 });
 
 module.exports = router;
